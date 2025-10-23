d1fdcf5039e2010e4c9d024f30b26e3545ca4d0a
Charilaos-Kanellopoulos <kanel.xaris@gmail.com>
2025-10-23 16:25:42 +0300

    final

---
+++ front/src/pages/Open369.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Open369: React.FC = () => {
+  return (
+    <div style={{padding:40}}>
+      <h1>Open369</h1>
+      <p>This is the /open369 placeholder page.</p>
+    </div>
+  );
+};
+
+export default Open369;
